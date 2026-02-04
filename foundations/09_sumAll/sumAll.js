ae2ba7e1b40024ecafcf146f5f1582aac09f1703
Robert Estrada <california.beatdown@icloud.com>
2026-02-04 09:09:26 -0800

    08-09 complete

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,7 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    
+    return Array.from({length: Math.abs(num2 - num1) + 1}, (_, i) => Math.min(num1, num2) + i)
+                .reduce((acc, curr) => acc + curr, 0);
 
 };
 
