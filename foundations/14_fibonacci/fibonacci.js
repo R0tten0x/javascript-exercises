dabc8a11c194b78da33007e338ca950b0603f926
Robert Estrada <california.beatdown@icloud.com>
2026-02-04 17:59:18 -0800

    14-16 completed

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(fibonaccin) {
+  if (fibonaccin < 0) {
+    throw new Error("Input must be a non-negative integer");
+  }
+  if (fibonaccin === 0) {
+    return 0;
+  }
+  if (fibonaccin === 1) {
+    return 1;
+  }
+  return fibonacci(fibonaccin - 1) + fibonacci(fibonaccin - 2);
 };
 
 // Do not edit below this line
--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  let oldestPerson = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    const currentPerson = arr[i];
+    const currentAge = currentPerson.yearOfDeath ? currentPerson.yearOfDeath - currentPerson.yearOfBirth : new Date().getFullYear() - currentPerson.yearOfBirth;
+    const oldestAge = oldestPerson.yearOfDeath ? oldestPerson.yearOfDeath - oldestPerson.yearOfBirth : new Date().getFullYear() - oldestPerson.yearOfBirth;
+    if (currentAge > oldestAge) {
+      oldestPerson = currentPerson;
+    }
+  }
+  return oldestPerson;
 };
 
 // Do not edit below this line
