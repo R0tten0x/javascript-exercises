d46b6b12184772d0475a24fbfd873bc9fa977797
Robert Estrada <california.beatdown@icloud.com>
2026-02-04 17:16:08 -0800

    12-13 completed

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((acc, curr) => acc + curr, 0);
+};  
+
+const multiply = function(numbers) {
+	return numbers.reduce((acc, curr) => acc * curr, 1);
 };
 
-const multiply = function() {
-
+const power = function(base, exponent) {
+	return Math.pow(base, exponent);
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number < 0) {
+		throw new Error("Factorial is not defined for negative numbers");
+	}
+	if (number === 0) {
+		return 1;
+	}
+	return number * factorial(number - 1);
 };
 
 // Do not edit below this line
--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const cleanString = string.toLowerCase().replace(/[^a-z]/g, '');
+  const reversedString = cleanString.split('').reverse().join('');
+  return cleanString === reversedString;
 };
 
 // Do not edit below this line
